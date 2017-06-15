639f17178c49ca5fa2d9f23fb8cd5775ffcfc1c1
tallroids <har05054@byui.edu>
2017-06-15 13:19:21 -0600

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+var model = require('./model.js')
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -9,12 +9,23 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/calcRate', function (request, response) {
+  var type = request.query.type;
+  var weight = Number(request.query.weight);
+
+  var rate = model.calcRate(type, weight);
+  if (rate == 0) {
+    response.render('pages/error');
+  }
+  var data = {
+    type: type,
+    weight: weight,
+    rate: rate
+  }
+
+  response.render('pages/index', data)
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
